4c166f8dd8116bdeb42cc7fe93e8340df12cceed
razvanyork <github@petertaylor.me>
2016-12-26 02:23:02 -0500

    Added an entrypoint for all commands (opendockvpn.sh)

--- opendockvpn.sh
+++ opendockvpn.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
 #
-echo "Reading config...."
-. ./dockervpn.config
+#if (( "$#" == 0 ))
+#then
+#        echo "Usage: $0 <shortname>"
+#        exit 1
+#fi
 
-echo "MAKING NEW CERTS BASED on ./dockervpn.config"
-for i in ${VPN_KEYS[@]}; do
-  docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $i nopass
-  docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $i > $i.ovpn
-done
+#echo "Reading config...."
+#. ./dockervpn.config
+
+case "$1" in
+        add)
+            bash addnewkey.sh "$2"
+            exit 0
+            ;;
+         
+        addfromfile)
+            bash addfromfile.sh
+            exit 0
+            ;;
+            
+        init)
+            bash initialize.sh
+            exit 0
+            ;;
+         
+        run)
+            bash runvpnserver.sh
+            exit 0
+            ;;
+        stop)
+            bash stopvpnserver.sh
+            exit 0
+            ;;
+         
+        *)
+            echo "Usage: $0 <command>, where <command> is:"
+            echo "  add SHORTNAME    adds and creates keyfile to an existing instance"
+            echo "  addfromfile      adds a key as specified in the dockervpn.config"
+            echo "  init             initializes and runs the vpn server"
+            echo "  run              runs the vpn server"
+            echo "  stop             stops the vpn server"
+            exit 1
+esac
